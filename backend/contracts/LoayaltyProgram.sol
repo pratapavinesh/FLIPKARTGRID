e8585eac586de16b9c76ac12d747d378bf6c6bdc
Avinesh Pratap Singh <pratapavinesh2003@gmail.com>
2023-08-20 01:21:40 +0530

    Adding Backend logic for loayalty program

--- backend/contracts/LoayaltyProgram.sol
+++ backend/contracts/LoayaltyProgram.sol
@@ -18,7 +18,7 @@ contract LoayaltyProgram is CosmosForLocal{
     using walletAlias for Wallet.wallet;
 
     // this will map the User with their address
-    mapping(address => person) public addressToUser;
+    mapping(address => personAlias) public addressToUser;
 
     // store the address
     address[] public keys;
@@ -36,14 +36,14 @@ contract LoayaltyProgram is CosmosForLocal{
     // This function will return the transactions of the sender
     function getTransactions( address _address ) public view returns (personAlias.transactionsActivity memory) {
         // Return the transactions of the sender
-        return addressToUser[_address].user.transactionsActivity;
+        return addressToUser[_address].transactionsActivity;
     }
 
     
     // This function will return the balance of the sender
     function getBalance() public view returns (uint256) {
         // Return the balance of the sender
-        return addressToUser[msg.sender].user.walletInformation.amount;
+        return addressToUser[msg.sender].walletInformation.amount;
     }
 
     function addPartner(personAlias memory _person ) public virtual override returns (bool success) {
@@ -83,18 +83,17 @@ contract LoayaltyProgram is CosmosForLocal{
     // This function will transfer the tokens from the sender to the receiver
     function transfer( address _to, uint256 _value ) public virtual override returns (bool success) {
         // Check if the sender has enough tokens
-        require(addressToUser[msg.sender].user.wallet.amount >= _value);
+        require(addressToUser[msg.sender].wallet.amount >= _value);
 
         // Subtract the tokens from the sender
-        addressToUser[msg.sender].user.wallet.amount -= _value;
+        addressToUser[msg.sender].wallet.amount -= _value;
 
         // Add the tokens to the receiver
-        addressToUser[_to].user.wallet.amount += _value;
+        addressToUser[_to].wallet.amount += _value;
 
         // Add the transaction to the transactions mapping
-        addressToUser[msg.sender].transactionsActivity.push(transactionAlias());
-        transactions[msg.sender].push(Transaction(msg.sender, _to, _value));
-        transactions[_to].push(Transaction(msg.sender, _to, _value));
+        addressToUser[msg.sender].transactionsActivity.push(transactionAlias(msg.sender, _to, _value));
+        addressToUser[_to].transactionsActivity.push(transactionAlias(msg.sender, _to, _value));
 
         // Emit the transfer event
         emit Transfer(msg.sender, _to, _value);
@@ -108,17 +107,17 @@ contract LoayaltyProgram is CosmosForLocal{
         // Check if the sender is the contract creator
         require(msg.sender == ownerAddres);
         // Check if the sender has enough tokens
-        require(balanceOf[_from] >= _value);
+        require( addressToUser[_from].wallet.amount >= _value);
 
         // Subtract the tokens from the sender
-        balanceOf[_from] -= _value;
+        addressToUser[_from].wallet.amount -= _value;
 
         // Add the tokens to the receiver
-        balanceOf[_to] += _value;
+        addressToUser[_to].wallet.amount += _value;
 
         // Add the transaction to the transactions mapping
-        transactions[_from].push(Transaction(_from, _to, _value));
-        transactions[_to].push(Transaction(_from, _to, _value));
+        addressToUser[_from].transactionsActivity.push(transactionAlias(_from, _to, _value));
+        addressToUser[_to].transactionsActivity.push(transactionAlias(_from, _to, _value));
 
         // Emit the transfer event
         emit Transfer(_from, _to, _value);
@@ -128,18 +127,18 @@ contract LoayaltyProgram is CosmosForLocal{
     }
 
     // This function will mint the tokens to the receiver
-    function mint(address _to, uint256 _value) public virtual returns (bool success) {
+    function mint(address _to, uint256 _value) public virtual override returns (bool success) {
         // Check if the sender is the contract creator
         require(msg.sender == ownerAddres);
 
         // Add the tokens to the receiver
-        balanceOf[_to] += _value;
+        addressToUser[_to].wallet.amount += _value;
 
         // Add the tokens to the total supply
         totalSupply += _value;
 
         // Add the transaction to the transactions mapping
-        transactions[_to].push(Transaction(address(0), _to, _value));
+        addressToUser[_to].transactionsActivity.push(transactionAlias(address(0), _to, _value));
 
         // Emit the transfer event
         emit Transfer(address(0), _to, _value);
@@ -149,20 +148,18 @@ contract LoayaltyProgram is CosmosForLocal{
     }
 
         // This function will burn the tokens from the sender
-    function burn(uint256 _value) public virtual returns (bool success) {
+    function burn(uint256 _value) public virtual override returns (bool success) {
         // Check if the sender has enough tokens
-        require(balanceOf[msg.sender] >= _value);
+        require(addressToUser[msg.sender].wallet.amount >= _value);
 
         // Subtract the tokens from the sender
-        balanceOf[msg.sender] -= _value;
+        addressToUser[msg.sender].wallet.amount -= _value;
 
         // Subtract the tokens from the total supply
         totalSupply -= _value;
 
         // Add the transaction to the transactions mapping
-        transactions[msg.sender].push(
-            Transaction(msg.sender, address(0), _value)
-        );
+         addressToUser[msg.sender].transactionsActivity.push(transactionAlias(msg.sender, address(0), _value));
 
         // Emit the transfer event
         emit Transfer(msg.sender, address(0), _value);
@@ -173,37 +170,32 @@ contract LoayaltyProgram is CosmosForLocal{
 
 
     // This function will reward the tokens to the sender
-    function reward( address _partner, uint256 _value ) public virtual returns (bool success) {
+    function reward( address _partner, uint256 _value ) public virtual override returns (bool success) {
         // require partner to be in authorised partners
         // Loop through the partners array
         bool hasPartner = false;
-        for (uint256 i = 0; i < partners.length; i++) {
-            // Check if the partner is found
-            if (partners[i].partner == _partner) {
-                hasPartner = true;
-            }
+        
+        if(addressToUser[_partner].utype=="partner"){
+            hasPartner=true;
         }
+
         require(hasPartner == true);
 
         // calculate the tokens according to value
         uint256 tokensValue = _value / 100;
 
         // Check if the partner has enough tokens
-        require(balanceOf[_partner] >= tokensValue);
+        require(addressToUser[_partner].wallet.amount >= tokensValue);
 
         // subtract the tokens from the partner
-        balanceOf[_partner] -= tokensValue;
+        addressToUser[_partner].wallet.amount -= tokensValue;
 
         // add the tokens to the sender
-        balanceOf[msg.sender] += tokensValue;
+        addressToUser[msg.sender].wallet.amount += tokensValue;
 
         // Add the transaction to the transactions mapping
-        transactions[msg.sender].push(
-            Transaction(_partner, msg.sender, tokensValue)
-        );
-        transactions[_partner].push(
-            Transaction(_partner, msg.sender, tokensValue)
-        );
+        addressToUser[msg.sender].transactionsActivity.push(transactionAlias(_partner, msg.sender, _value));
+        addressToUser[_partner].transactionsActivity.push(transactionAlias(_partner, msg.sender, _value));
 
         // return true
         return true;
